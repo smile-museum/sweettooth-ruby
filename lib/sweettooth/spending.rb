a927d5faa4f06802892370fb9a1081442e383d1f
Bill Curtis <wcurtis@sweettoothhq.com>
2014-01-29 16:25:26 -0500

    Added cancelable spendings

--- lib/sweettooth/spending.rb
+++ lib/sweettooth/spending.rb
@@ -1,5 +1,15 @@
 module SweetTooth
   class Spending < APIResource
     include SweetTooth::APIOperations::Create
+
+    def cancel()
+      response, api_key = SweetTooth.request(:post, cancel_url, @api_key)
+      refresh_from(response, api_key)
+      self
+    end
+
+    def cancel_url
+      url + '/cancel'
+    end
   end
 end
